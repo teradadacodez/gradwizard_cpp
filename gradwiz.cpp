c97c44ac274c29b8340ee887f3900b06799a3782
Tanmay <tanmayjain10304@gmail.com>
2025-12-20 22:30:25 +0530

    initial commit

---
+++ gradwiz.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+    double data ; 
+    double grad ;
+    string op ;
+    function<void()> _backward ;
+    set<node> parents
+    
+    public : 
+    node(int d, int g = 0, string l = "") : data(d), grad(g), op(l)
+    {
+        _backward = [](){} ;
+        cout << "node constructor" << endl;
+    }
+    ~node() {cout << "node destructor" << endl;}
+    void show() {cout << "(data,grad,op) : (" << data << ", " << grad << ", " << op << ")" << endl;}
+    node operator+(node& other)
+    {
+        node out = node(data+other.data,0,"+") ;
+        _backward = [this,other,out]()
+        {
+            this->grad += 1*out.grad ;
+            other.grad += 1*out.grad ;
+        }
+        return out ;
+    }
+    node operator-(node& other)
+    {
+        
+        return node(data-other.data,0,"-") ;
+    }
+    node operator*(node& other)
+    {
+        return node(data*other.data,0,"*") ;
+    }
+    node operator/(node& other)
+    {
+        return node(data/other.data,0,"/") ;
+    }
+    friend ostream& operator<<(ostream& COUT, const node& other) ;
+};
+
+ostream& operator<<(ostream& COUT, const node& other)
+{
+    COUT << other.data << ", " << other.grad << ", " << other.op << endl;
+    return COUT ;
+}
+int main()
+{   
+    node a(10), b(20) ;
+    node c = a+b ;
+    c.show() ;
+    cout << c << endl;
+    cout << a+b << endl;  
+}
---
+++ temp.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node : public enable_shared_from_this<node>
+{
+    double data ; 
+    double grad ;
+    string op ;
+    function<void()> _backward ;
+    set<node> parents ;
+
+    public : 
+    node(double d, string o = "") : data(d), grad(0), op(o), _backward([](){})
+    {
+        cout << "node constructor" << endl;
+    }
+    double getdata() const {return data ;}
+    double getgrad() const {return grad ;}
+
+    friend shared_ptr<node> operator+(shared_ptr<node> self, shared_ptr<node> other) ;
+    friend shared_ptr<node> operator*(shared_ptr<node> self, shared_ptr<node> other) ;
+
+
+    void backward()
+    {
+        grad = 1.0 ;
+        _backward() ;
+    }
+    
+};
+
+shared_ptr<node> operator+(shared_ptr<node> self, shared_ptr<node> other)
+{
+    auto out = make_shared<node>(self->data+other->data,"+");
+    out->_backward = [self,other,out]()
+    {
+        self->grad += out->grad ;
+        other->grad += out->grad ;
+    };
+    return out ;
+}
+shared_ptr<node> operator*(shared_ptr<node> self, shared_ptr<node> other)
+{
+    auto out = make_shared<node>(self->data*other->data,"*") ;
+    out->_backward = [self,other,out]()
+    {
+        self.grad += 
+    }
+}
+
+int main()
+{
+    auto a {make_shared<node>(2.0)}, b {make_shared<node>(3)} ;
+    auto c = a*b ;
+    c->backward() ;
+    cout << a->getgrad() << " " << b->getgrad() << endl;
+}
